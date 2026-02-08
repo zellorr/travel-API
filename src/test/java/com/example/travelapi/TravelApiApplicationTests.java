7fe6e11d5a8b1a6e518b53fa744d34289eb1638c
Ulan Akhmet <ulanahmet5@gmail.com>
2026-02-08 22:26:36 +0500

    Initial commit

---
+++ src/test/java/com/example/travelapi/TravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
